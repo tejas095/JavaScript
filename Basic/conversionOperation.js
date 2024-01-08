d45af2930a91749af263461ed43502d46d396583
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-08 16:35:05 +0000

    Operation & Comperision

--- Basic/conversionOperation.js
+++ Basic/conversionOperation.js
@@ -22,5 +22,38 @@ let booleanIsLoggedIn = Boolean(isLoggefIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof someNumber);
+//console.log(someNumber);
+//console.log(typeof someNumber);
+/*------------------------------------Operations----------------------------------------------*/
+
+let value =3
+let negValue =-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 =" tejas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log( (3 + 4) * 5 % 3);
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
