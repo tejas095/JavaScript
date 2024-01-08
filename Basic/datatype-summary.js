e9fdf2ce4a399c7fdd536d034d2f3280ae934459
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-08 17:08:30 +0000

    DataType Summary

---
+++ Basic/datatype-summary.js
@@ -0,0 +1,34 @@
+// Primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+let userEmail1=undefined;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id===anotherId);
+
+const bigNumber = 31542255221144554n
+
+
+// Referance Type( Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj = {
+    name:"tejas",
+    age:28,
+}
+
+const myFunction=function(){
+    console.log("Hello Wolrd");
+}
+
+console.log(typeof anotherId);
