f512c34299b2ff510082771e04507e7f907be107
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-08 11:02:09 +0000

    DataType

---
+++ Basic/datatype.js
@@ -0,0 +1,27 @@
+"Use Strict"; // Treat All JS Code as newer version.
+
+// alert (3+5); // We are usging node js, not browser.
+
+console.log(3+3)
+console.log("Tejas")
+
+let name = "tejas"
+let age = 28
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => Null , Empty Value
+// symbol => unique
+
+
+// object
+console.log(typeof "tejas");
+console.log(typeof 28)
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
