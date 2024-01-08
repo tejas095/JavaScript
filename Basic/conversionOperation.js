9dd2e6dfdd179b2b3b3a8cba4545fb221295edbe
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-08 15:15:30 +0000

    Conversion Of DataType

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "tejas"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false =>0
+
+let isLoggefIn ="tejas"
+
+let booleanIsLoggedIn = Boolean(isLoggefIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false
+// ""=> false
+// "tejas" =>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
