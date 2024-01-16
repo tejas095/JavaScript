4b341cf0fca701062f144b7d3fdcfe83e925ff3c
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-16 16:39:51 +0000

    Funcations & Scopes

--- 3_Basic/1_functions.js
+++ 3_Basic/1_functions.js
@@ -30,4 +30,30 @@ function loginUserMessage(username ="TEJ") {
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Tejas"));
-console.log(loginUserMessage("Raj"));
+// console.log(loginUserMessage("Raj"));
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,5000));
+
+const user = {
+    username :"Tejas",
+    price : 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:"tej",
+    price:399
+})
+
+const myNewArray =[200,400,100,600]
+function retunSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(retunSecondValue(myNewArray));
+console.log(retunSecondValue([200,500,500,600]));
