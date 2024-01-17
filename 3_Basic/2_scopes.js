01a32001d7dab123be368ba4babfef79fe23f4a4
tejas095 <122140216+tejas095@users.noreply.github.com>
2024-01-17 09:31:44 +0000

    Scopes

--- 3_Basic/2_scopes.js
+++ 3_Basic/2_scopes.js
@@ -1,12 +1,46 @@
 
 let a = 300
 
-if(true){
-    let a=10
-    const b=20 
-    console.log("INNER:",a);  
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER:", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+function one() {
+    const username = "tejas"
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+if(true)
+{
+    const username = "tejas"
+    if(username==="tejas")
+    {
+        const website ="youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//++++++++++++++++++++++++++++++++Interesting++++++++++++++++++++++++++++++++++++++++++//
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1
+}
+
+// console.log(addTwo(5)); Not Use Upper Side in Const funcation
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
